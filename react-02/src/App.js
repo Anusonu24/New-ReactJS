01e910e0862918034d8b7b327b1edd02a7c20b9b
Anusonu24 <anurangu996@gmail.com>
2023-04-28 09:40:49 +0530

    ReactExamples....

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Services from './Components/Services'
+import Contact from './Components/Contact'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/index" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/services" component={Services} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ react-02/src/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className='navbar navbar-dark navbar-expand-lg'>
+               <Link to="/index" className="navbar-brand">Rounting Examples</Link>
+               <div className='ml-auto'>
+                <ul className='navbar-nav'>
+                <li className='nav-list'><Link to="/index" className="nav-link">Home</Link></li>
+                <li className='nav-list'><Link to="/about" classname="nav-link">About</Link></li>
+                <li classname='nav-list'><Link to="/service" classname="nav-link">service</Link></li>
+                <li classname='nav-list'><Link to="/contact" classname="nav-link">Contact</Link></li>
+                </ul>
+               </div>
+            </nav>
+        )
+
+        }
+    }
+    export default Navbar
