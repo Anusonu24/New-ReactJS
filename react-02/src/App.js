f3e9577b166df4e8fb839cb64b3465282d8b5e2a
Anusonu24 <anurangu996@gmail.com>
2023-05-03 11:53:04 +0530

    Loginexample

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import Navbar from './Navbar/Navbar'
-import Home from './Components/Home'
-import About from './Components/About'
-import Services from './Components/Services'
-import Contact from './Components/Contact'
-import User from'./User/User'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Route, Switch } from 'react-router-dom'/cjs/react-router-DOMException.min'
-function App() {
+import Login from './login/login'
+const App = () => {
     return (
         <div>
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route path="/index" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/services" component={Services} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/User" component={User} />
-                </Switch>
-            </Router>
-        </div>
+            <h1> App Component</h1>
+            <hr/>
+            <login/>
+                    </div>
     )
 }
-
 export default App
